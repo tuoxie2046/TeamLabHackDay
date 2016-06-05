238e90600548a4c5632a23c04855e314a99ca1a0
tuoxie2046 <jinze.yu.kp@gmail.com>
2016-06-05 11:39:30 +0900

    students

--- Assets/keyboardcontrol.cs
+++ Assets/keyboardcontrol.cs
@@ -3,15 +3,25 @@ using System.Collections;
 
 public class keyboardcontrol : MonoBehaviour {
 
-	public
+	public Transform lighting;
+	private float lightime;
 
 	// Use this for initialization
 	void Start () {
-	
+		lightime = 0.0f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		//if 
+		if (Input.GetKey ("a")) {
+			if(lightime == -1.0f)
+				Transform cloneLighting = Instantiate(lighting, new Vector3(0, 0, 0), Quaternion.identity) as Transform;
+			else if (lightime > 0)
+				lightime -= Time.deltaTime;
+			if(lightime == 0){
+				//Destroy(cloneLighting);
+
+			}
+		}
 	}
 }
