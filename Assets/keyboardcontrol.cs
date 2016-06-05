a231bffa2815766d1b5aac261a67aac1b67669c1
tuoxie2046 <jinze.yu.kp@gmail.com>
2016-06-05 11:05:25 +0900

    add lighting etc. classroom

---
+++ Assets/keyboardcontrol.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class keyboardcontrol : MonoBehaviour {
+
+	public
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//if 
+	}
+}
